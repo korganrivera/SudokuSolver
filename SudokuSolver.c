86cb6d419725744a3825568316b8f182172e6b44
korganrivera <korganrivera@gmail.com>
2015-06-28 01:41:40 -0500

    shorter code

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -7,68 +7,42 @@
 #include <stdlib.h>
 
 unsigned grid[9][9]={
-    {4,5,0,1,0,6,0,0,0},
-    {0,0,0,0,0,0,0,0,3},
-    {0,0,6,0,7,0,9,0,0},
-    {5,0,0,3,0,0,6,9,0},
-    {0,0,0,0,8,0,0,0,0},
-    {0,6,4,0,0,2,0,0,1},
-    {0,0,1,0,5,0,2,0,0},
     {8,0,0,0,0,0,0,0,0},
-    {0,0,0,8,0,1,0,5,4}
+    {0,0,3,6,0,0,0,0,0},
+    {0,7,0,0,9,0,2,0,0},
+    {0,5,0,0,0,7,0,0,0},
+    {0,0,0,0,4,5,7,0,0},
+    {0,0,0,1,0,0,0,3,0},
+    {0,0,1,0,0,0,0,6,8},
+    {0,0,8,5,0,0,0,1,0},
+    {0,9,0,0,0,0,4,0,0}
 };
 
 void func(unsigned x);
 void show(void);
 
 int main(void){
-    puts("\nproblem:");
+    puts("\n problem:");
     show();
     func(0);
+    puts("\n unsolvable.");
 }
 
 void func(unsigned x){
     unsigned quadindex, i, list[9]={1,1,1,1,1,1,1,1,1};
 
     //  if at element 81, the grid is complete. display and end.
-    if(x==81){ puts("\nsolution:"); show(); exit(0); }
+    if(x==81){ puts("\n solution:"); show(); exit(0); }
 
     //  if element isn't zero, skip over it.
     if(grid[x/9][x%9]){ func(x+1); return; }
 
+    quadindex = 3*(x/27) + (x/3)%3;                 //  get quad's index.
+
     for(i=0; i<9; i++){
         if(grid[i][x%9]) list[grid[i][x%9]-1]=0;    //  check current column.
         if(grid[x/9][i]) list[grid[x/9][i]-1]=0;    //  check current row.
-    }
-
-    quadindex = 3*(x/27) + (x/3)%3;                 //  get quad's index.
-
-    if(quadindex==0){
-        for(i=0; i<9; i++) if(grid[i/3][i%3]) list[grid[i/3][i%3]-1]=0;
-    }
-    else if(quadindex==1){
-       for(i=0; i<9; i++) if( grid[i/3][i%3+3]) list[grid[i/3][i%3+3]-1]=0;
-    }
-    else if(quadindex==2){
-       for(i=0; i<9; i++) if( grid[i/3][i%3+6]) list[grid[i/3][i%3+6]-1]=0;
-    }
-    else if(quadindex==3){
-       for(i=0; i<9; i++) if( grid[i/3+3][i%3]) list[grid[i/3+3][i%3]-1]=0;
-    }
-    else if(quadindex==4){
-       for(i=0; i<9; i++) if( grid[i/3+3][i%3+3]) list[grid[i/3+3][i%3+3]-1]=0;
-    }
-    else if(quadindex==5){
-       for(i=0; i<9; i++) if( grid[i/3+3][i%3+6]) list[grid[i/3+3][i%3+6]-1]=0;
-    }
-    else if(quadindex==6){
-       for(i=0; i<9; i++) if( grid[i/3+6][i%3]) list[grid[i/3+6][i%3]-1]=0;
-    }
-    else if(quadindex==7){
-       for(i=0; i<9; i++) if( grid[i/3+6][i%3+3]) list[grid[i/3+6][i%3+3]-1]=0;
-    }
-    else if(quadindex==8){
-       for(i=0; i<9; i++) if( grid[i/3+6][i%3+6]) list[grid[i/3+6][i%3+6]-1]=0;
+        if(grid[i/3+(quadindex/3)*3][i%3+(quadindex%3)*3]) list[grid[i/3+(quadindex/3)*3][i%3+(quadindex%3)*3]-1]=0; //  check current quad.
     }
 
     //  now all the numbers that element x could be are noted in list[].
@@ -89,6 +63,6 @@ void func(unsigned x){
 
 void show(void){
     unsigned i;
-    for(i=0; i<81; i++){ if(!(i%9)) putchar('\n'); printf("%u ", grid[i/9][i%9]); }
+    for(i=0; i<81; i++){ if(!(i%9)) putchar('\n'); printf(" %u", grid[i/9][i%9]); }
     putchar('\n');
 }
